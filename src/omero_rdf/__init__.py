54e6cb44b330cf77943028fdc9499e6e09c29643
Josh Moore <josh@openmicroscopy.org>
2024-12-05 23:00:56 +0100

    0.5.1: correct merge error
    The print statement introduced in the merge commit
    needed to be converted to a `return` in the new
    formatting strategy.

--- src/omero_rdf/__init__.py
+++ src/omero_rdf/__init__.py
@@ -137,7 +137,7 @@ class NTriplesFormat(StreamingFormat):
     def serialize_triple(self, triple):
         s, p, o = triple
         escaped = o.n3().encode("unicode_escape").decode("utf-8")
-        print(f"""{s.n3()}\t{p.n3()}\t{escaped} .""")
+        return f"""{s.n3()}\t{p.n3()}\t{escaped} ."""
 
 
 class NonStreamingFormat(Format):
