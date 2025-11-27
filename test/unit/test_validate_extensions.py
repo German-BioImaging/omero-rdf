79f1419a6f95fb634f3879b88ae1d86ebb994b76
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-11-27 16:46:39 -0300

    Add unit tests for extension validation

---
+++ test/unit/test_validate_extensions.py
@@ -0,0 +1,45 @@
+import logging
+from types import SimpleNamespace
+
+from omero_rdf import RdfControl
+
+
+class DummyCtx:
+    def out(self, msg):
+        pass
+
+    def err(self, msg):
+        pass
+
+
+def test_warns_for_nt_when_turtle(caplog):
+    ctrl = RdfControl()
+    ctrl.ctx = DummyCtx()
+    args = SimpleNamespace(file="out.nt", format="turtle", pretty=False, yes=True)
+
+    with caplog.at_level(logging.WARNING):
+        ctrl._validate_extensions(args)
+
+    assert ".nt' does not match format 'turtle'" in caplog.text
+
+
+def test_warns_when_pretty_overrides_extension(caplog):
+    ctrl = RdfControl()
+    ctrl.ctx = DummyCtx()
+    args = SimpleNamespace(file="out.nt", format="ntriples", pretty=True, yes=True)
+
+    with caplog.at_level(logging.WARNING):
+        ctrl._validate_extensions(args)
+
+    assert ".turtle' does not match format 'ntriples'" in caplog.text
+
+
+def test_allows_matching_extension(caplog):
+    ctrl = RdfControl()
+    ctrl.ctx = DummyCtx()
+    args = SimpleNamespace(file="out.nt", format="ntriples", pretty=False, yes=True)
+
+    with caplog.at_level(logging.WARNING):
+        ctrl._validate_extensions(args)
+
+    assert not caplog.records
